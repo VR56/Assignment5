f44a67fe3ec93d4282b04a01f1e76d93eb5362cf
VR56 <53446898+VR56@users.noreply.github.com>
2020-01-22 12:33:41 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project_1
+{
+    /* Class for Arithmetic Opertions*/
+
+    /// <summary>
+    /// The Arithmetic class
+    /// Contains all the basic arithmetic methods
+    /// </summary>
+    /// <remarks>
+    /// <para>This class has methods for Addition, Substraction, Multiplication and Divison.</para>
+    /// </remarks>
+
+    ///<list type="bullet">
+    /// <item>
+    /// <term>Add</term>
+    /// <description>Addition Operation</description>
+    /// </item>
+    /// <item>
+    /// <term>Subtract</term>
+    /// <description>Subtraction Operation</description>
+    /// </item>
+    /// <item>
+    /// <term>Multiply</term>
+    /// <description>Multiplication Operation</description>
+    /// </item>
+    /// <item>
+    /// <term>Divide</term>
+    /// <description>Division Operation</description>
+    /// </item>
+    /// </list>
+ 
+
+    public class Arithmetic
+    {
+        /// <summary>
+        /// Adds two numbers and returns the sum
+        /// </summary>
+        /// <param name="Number1">An intger type</param>
+        /// <param name="Number2">An intger type</param>
+        ///<returns>
+        ///<para>returns the sum of two numbers</para>
+        ///</returns>
+        ///<example>
+        ///<code>
+        ///int sum=Arithmetic.Add(9,10)
+        ///Console.WriteLine(sum);
+        ///</code>
+        /// </example>
+        /// <exception cref="OverflowException">Occurs when one of the number is more than the integer value</exception>
+        public int Add(int Number1, int Number2)
+        {
+            if ((Number1 == int.MaxValue && Number2 > 0) || (Number2 == int.MaxValue && Number1 > 0))
+                throw new System.OverflowException();
+
+            return Number1 + Number2;
+        }
+
+
+        /// <summary>
+        /// Substracts two numbers and returns the difference
+        /// </summary>
+        /// <param name="Number1">An intger type</param>
+        /// <param name="Number2">An intger type</param>
+        /// <returns>
+        /// <para>returns of difference of two numbers</para>
+        /// </returns>
+        ///<example>
+        ///<code>
+        ///int Difference=Arithmetic.Substract(19,10)
+        ///Console.WriteLine(Difference);
+        ///</code>
+        ///</example>
+        public int Substract(int Number1, int Number2)
+        {
+            return Number1 - Number2;
+        }
+
+
+        /// <summary>
+        /// Multiplies two numbers and returns the product
+        /// </summary>
+        /// <param name="Number1">An intger type</param>
+        /// <param name="Number2">An intger type</param>
+        /// <returns>
+        /// <para>returns the product of two numbers</para>
+        /// </returns>
+        ///<example>
+        ///<code>
+        ///int Product=Arithmetic.Multiply(9,10)
+        ///Console.WriteLine(Product);
+        ///</code>
+        ///</example>
+        ///<exception cref="OverflowException">Occurs when one of the number is more than the integer value</exception>
+
+        public int Multiply(int Number1, int Number2)
+        {
+            if ((Number1 == int.MaxValue && Number2 > 0) || (Number2 == int.MaxValue && Number1 > 0))
+                throw new System.OverflowException();
+
+            return Number2 * Number1;
+        }
+
+        /// <summary>
+        /// Divides two numbers and returns the quotient
+        /// </summary>
+        /// <param name="Number1">An intger type</param>
+        /// <param name="Number2">An intger type</param>
+        /// <returns>
+        /// <para>returns the quotient of two numbers</para>
+        /// </returns>
+        ///<example>
+        ///<code>
+        ///int Quotient=Arithmetic.Divide(9,10)
+        ///Console.WriteLine(Quotient);
+        ///</code>
+        ///</example>
+        ///<exception cref="DivideByZeroException"> Occurs when a number is attemted to be divided by zero</exception>
+        public int Divide(int Number1, int Number2)
+        {
+            if (Number2 == 0)
+                throw new DivideByZeroException();
+                    
+            return Number1 / Number2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Project_1
+{
+    /// <summary>
+    /// This is the main class where all the methods are declared and implemented.
+    /// </summary>
+    /// <remarks>
+    /// This class can add and print the sum.
+    /// </remarks>
+    class Program
+    {
+        public static void Main()
+        {
+            Arithmetic obj = new Arithmetic();
+            int number_1, number_2;
+            int sum = 0, Difference=0, Product=0,Quotient=0;
+            Console.WriteLine("Enter the first number");
+            number_1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            number_2 = Convert.ToInt32(Console.ReadLine());
+
+            sum = obj.Add(number_1, number_2);
+            Difference = obj.Substract(number_1, number_2);
+            Product = obj.Multiply(number_1, number_2);
+            Quotient = obj.Divide(number_1, number_2);
+
+            Console.WriteLine("The sum is "+sum+"   The Difference is "+Difference+"   The Product is "+Product+"   The Quotient is "+Quotient);
+            Console.ReadLine();
+  
+        }
+
+
+    }
+}
